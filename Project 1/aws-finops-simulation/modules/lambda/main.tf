34463ca7a72a35f9568f9b19dfe1955f62c4c503
Swapnil Lakra <swapnillakra822@gmail.com>
2026-04-27 08:15:26 +0530

    Create lambda, and iam role for lambda, and iam role policy for lambda

--- Project 1/aws-finops-simulation/modules/lambda/main.tf
+++ Project 1/aws-finops-simulation/modules/lambda/main.tf
@@ -15,5 +15,53 @@ resource "aws_iam_role" "alarm_lambda_role" {
 }
 
 resource "aws_iam_role_policy" "alarm_lambda_role" {
-  
+  name = "alarm-action-policy"
+  role = aws_iam_role.alarm_lambda_role.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+        {
+            Effect = "Allow"
+            Action = [
+                "logs:CreateLogGroup",
+                "logs:CreateLogStream",
+                "logs:PutLogEvents"
+            ]
+            Resource = "arn:aws:logs:*:*:*"
+        },
+        {
+            Effect = "Allow"
+            Action = [
+                "ec2:StopInstances",
+                "ec2:DescribeInstances"
+            ]
+            Resource = "*"
+        },
+        {
+            Effect = "Allow"
+            Action = [
+               "rds:StopDBInstance",
+               "rds:ModifyDBInstance",
+               "rds:DescribeDBInstance"
+            ]
+            Resource = "*"
+        },
+        {
+            Effect = "Allow"
+            Action = [
+               "s3:PutBucketLifecycleConfiguration",
+               "s3:GetBucketLifecycleConfiguration",
+               "s3:ListBucket"
+            ]
+            Resource = "arn:aws:s3:::${var.s3_bucket_name}"
+        },
+        {
+            Effect = "Allow"
+            Action = [
+                "s3:GetBucketLocation"
+            ]
+            Resource = "arn:aws:s3:::${var.s3_bucket_name}"
+        }
+    ]
+  })
 }
---
+++ Project 1/aws-finops-simulation/modules/lambda/variables.tf
@@ -0,0 +1,14 @@
+variable "ec2_instances_id" {
+  description = "Id of EC2 instance"
+  type =  string
+}
+
+variable "rds_instance_identifier" {
+  description = "Unique identifier of rds instance"
+  type = string
+}
+
+variable "s3_bucket_name" {
+  description = "Name of S3 Bucket"
+  type = string
+}
